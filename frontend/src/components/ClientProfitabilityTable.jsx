08a3892af0863af740d059ad8feaa28eda1a0d7a
Javeriya Shaikh <54635656+Shaikh-Javeriya@users.noreply.github.com>
2025-09-28 17:06:23 +0530

    Update ClientProfitabilityTable.jsx

--- frontend/src/components/ClientProfitabilityTable.jsx
+++ frontend/src/components/ClientProfitabilityTable.jsx
@@ -4,15 +4,7 @@ import { format } from 'date-fns';
 import { useCurrency } from "./CurrencyContext";
 
 const ClientProfitabilityTable = ({ data }) => {
-  const { currency, locale } = useCurrency();
-  const formatCurrency = (value, options = {}) => {
-    return new Intl.NumberFormat(locale, {
-      style: "currency",
-      currency,
-      minimumFractionDigits: options.minFractionDigits ?? 0,
-      notation: options.notation || "standard",
-    }).format(value || 0);
-  };
+  const { currency, locale, formatCurrency } = useCurrency();
 
   const formatNumber = (num) => {
     return new Intl.NumberFormat('en-US', {
